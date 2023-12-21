4c13d7caec3ec5467d8679f631eb171160dc2394
Brandon Kwamou <brandonkwamou@gmail.com>
2023-12-21 11:25:22 +0100

    epitech strapi and vuejs

---
+++ front/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <MoodTracker />
+  </div>
+</template>
+
+<script>
+import MoodTracker from './components/MoodTracker.vue';
+export default {
+  name: 'App',
+  components: {
+    MoodTracker,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/styles.scss';
+</style>
+
